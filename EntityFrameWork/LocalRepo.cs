0e457435427ce9190c5b92b1709d3e6c823d2696
Aussien A Amin <122715805+hussienAmin@users.noreply.github.com>
2024-11-13 17:01:50 +0200

    Update LocalRepo.cs

--- EntityFrameWork/LocalRepo.cs
+++ EntityFrameWork/LocalRepo.cs
@@ -46,73 +46,10 @@ namespace KafApp.Repo.Server
             return await transAction.Excute();
         }
 
-        public async Task<KafQueryResult> AddBonusToCustomerOrder(CustomerOrderBonus md)
-        {
-            transAction.action = async () =>
-            {
-                md.Validate();
-                var Customer = await db.CustomerOrders.SingleAsync(j => j.Id == md.ParentId);
-                await SubtractQunt(md.ProductUnitId, Customer.StoreId, md.UnitQuntity);
+        
+        
 
-                await db.CustomerOrderBonus.AddAsync(md);
-                await db.SaveChangesAsync();
-
-
-                return await db.CustomerOrderBonus.SingleAsync(k => k.Id == md.Id);
-
-            };
-            return await transAction.Excute();
-        }
-
-        public async Task<KafQueryResult> AddBonusToSupply(SupplyBonus md)
-        {
-            transAction.action = async () =>
-            {
-                md.Validate();
-                var Supply = await db.Supplys.SingleAsync(j => j.Id == md.ParentId);
-                await AddQunt(md.ProductUnitId, Supply.StoreId, md.UnitQuntity);
-                await db.SupplyBonus.AddAsync(md);
-                await db.SaveChangesAsync();
-                return await db.SupplyBonus.SingleAsync(k => k.Id == md.Id);
-
-            };
-            var rs = await transAction.Excute();
-            return rs;
-
-        }
-
-        public async Task<KafQueryResult> AddItemToCashOrder(CashOrderItem md)
-        {
-            transAction.action = async () =>
-            {
-                md.Validate();
-                var Cash = await db.CashOrders.SingleAsync(j => j.Id == md.ParentId);
-
-                md.UnitCost = await CalcUnitCost(md.ProductUnitId);
-                await SubtractQunt(md.ProductUnitId, Cash.StoreId, md.UnitQuntity, md.ItemCost);
-
-
-                Cash.Total = Cash.Total.KAfAdd(md.Value);
-                Cash.TotalCost = Cash.TotalCost.KAfAdd(md.ItemCost);
-                Cash.ChecKCashOrderConstraints();
-
-                FinancialCycle cycle = await db.Sessions.Where(o => o.Id == Cash.SessionId).Select(j => j.FinancialCycle).SingleOrDefaultAsync() ?? throw new KafCustomException(KafQueryStatus.NoOpenCycle);
-                cycle.Stock = cycle.Stock.KAfSubtract(md.ItemCost);
-                cycle.Cash = cycle.Cash.KAfAdd(md.Value);
-                cycle.CashOrdersProfit = cycle.CashOrdersProfit.KAfAdd(md.ItemProf);
-
-
-
-                await db.CashOrderItems.AddAsync(md);
-                await db.SaveChangesAsync();
-
-
-                return await db.CashOrderItems.SingleAsync(k => k.Id == md.Id);
-
-            };
-            var rs = await transAction.Excute();
-            return rs;
-        }
+        
 
         public async Task<KafQueryResult> AddItemToCustomerOrder(CustomerOrderItem md)
         {
@@ -154,89 +91,8 @@ namespace KafApp.Repo.Server
             return rs;
         }
 
-        public async Task<KafQueryResult> AddItemToSupply(SupplyItem md)
-        {
-            transAction.action = async () =>
-            {
-                md.Validate();
-                var Supply = await db.Supplys.SingleAsync(j => j.Id == md.ParentId);
-                Supply.Total = Supply.Total.KAfAdd(md.Value);
-
-                var Supplier = await db.Suppliers.SingleAsync(j => j.Id == Supply.ParentId);
-                Supplier.SupplyTotal = Supplier.SupplyTotal.KAfAdd(md.Value);
-
-                await AddQunt(md.ProductUnitId, Supply.StoreId, md.UnitQuntity, md.Value);
-
-                FinancialCycle cycle = await db.Sessions.Where(o => o.Id == Supply.SessionId).Select(j => j.FinancialCycle).SingleOrDefaultAsync() ?? throw new KafCustomException(KafQueryStatus.NoOpenCycle);
-                cycle.SupplierDebts = cycle.SupplierDebts.KAfAdd(md.Value);
-                cycle.Stock = cycle.Stock.KAfAdd(md.Value);
-
-                await db.SupplyItems.AddAsync(md);
-                await db.SaveChangesAsync();
-
-
-                return await db.SupplyItems.SingleAsync(k => k.Id == md.Id);
-
-            };
-            var rs = await transAction.Excute();
-            return rs;
-        }
-
-        public async Task<KafQueryResult> AddNewCapitalDeposit(CapitalDeposit md)
-        {
-            transAction.action = async () =>
-            {
-                Partner partner = await db.Partners.Where(k => k.Id == md.PartnerId).SingleAsync();
-                partner.Credit = partner.Credit.KAfAdd(md.Amount);
-
-
-
-                PaymentOption pay = await db.Payments.Where(k => k.Id == md.PaymentId).SingleAsync();
-                pay.Credit = pay.Credit.KAfAdd(md.Amount);
-
-                FinancialCycle cycle = await db.Sessions.Where(o => o.Id == md.SessionId).Select(j => j.FinancialCycle).SingleOrDefaultAsync() ?? throw new KafCustomException(KafQueryStatus.NoOpenCycle);
-                cycle.Capital = cycle.Capital.KAfAdd(md.Amount);
-                cycle.Cash = cycle.Cash.KAfAdd(md.Amount);
-
-                await db.Set<CapitalDeposit>().AddAsync(md);
-                await db.SaveChangesAsync();
-
-                return await db.Set<CapitalDeposit>().SingleAsync(k => k.Id == md.Id);
-
-            };
-            var rs = await transAction.Excute();
-            return rs;
-        }
-
-        public async Task<KafQueryResult> AddNewCapitalWithdrawal(CapitalWithdrawal md)
-        {
-            transAction.action = async () =>
-            {
-                Partner partner = await db.Partners.Where(k => k.Id == md.PartnerId).SingleAsync();
-                partner.Credit = partner.Credit.KAfSubtract(md.Amount);
-                partner.ChecKPartnerConstraint();
-
-
-                PaymentOption pay = await db.Payments.Where(k => k.Id == md.PaymentId).SingleAsync();
-                pay.Credit = pay.Credit.KAfSubtract(md.Amount);
-                pay.ChecKPaymentConstraint();
-
-                FinancialCycle cycle = await db.Sessions.Where(o => o.Id == md.SessionId).Select(j => j.FinancialCycle).SingleOrDefaultAsync() ?? throw new KafCustomException(KafQueryStatus.NoOpenCycle);
-
-
-                cycle.Capital = cycle.Capital.KAfSubtract(md.Amount);
-                cycle.Cash = cycle.Cash.KAfSubtract(md.Amount);
-                await db.Set<CapitalWithdrawal>().AddAsync(md);
-                await db.SaveChangesAsync();
-
-                return await db.Set<CapitalWithdrawal>().SingleAsync(k => k.Id == md.Id);
-
-            };
-            var rs = await transAction.Excute();
-            return rs;
-        }
-
-        public async Task<KafQueryResult> AddNewCashOrder(CashOrder md)
+        
+          public async Task<KafQueryResult> AddNewCashOrder(CashOrder md)
         {
             transAction.action = async () =>
             {
@@ -303,51 +159,4 @@ namespace KafApp.Repo.Server
             return rs;
         }
 
-        public async Task<KafQueryResult> AddNewCustomerOrder(CustomerOrder md)
-        {
-            transAction.action = async () =>
-            {
-                md.Validate();
-
-                var Customer = await db.Customers.SingleAsync(j => j.Id == md.ParentId);
-                Customer.CustomerOrderTotal = Customer.CustomerOrderTotal.KAfAdd(md.Rest);
-                Customer.ChecKCustomerConstraints();
-                foreach (var item in md.ItemsList)
-                {
-                    item.UnitCost = await CalcUnitCost(item.ProductUnitId);
-                    await SubtractQunt(item.ProductUnitId, md.StoreId, item.UnitQuntity, item.ItemCost);
-                    md.TotalCost = md.TotalCost.KAfAdd(item.ItemCost);
-
-                }
-                foreach (var item in md.BonusList)
-                {
-                    await SubtractQunt(item.ProductUnitId, md.StoreId, item.UnitQuntity);
-
-                }
-
-                FinancialCycle cycle = await db.Sessions.Where(o => o.Id == md.SessionId).Select(j => j.FinancialCycle).SingleOrDefaultAsync() ?? throw new KafCustomException(KafQueryStatus.NoOpenCycle);
-                cycle.CustomersDebts = cycle.CustomersDebts.KAfAdd(md.Rest);
-                cycle.Stock = cycle.Stock.KAfSubtract(md.TotalCost);
-                cycle.CustomerOrdersProfit = cycle.CustomerOrdersProfit.KAfAdd(md.TotalProf);
-                cycle.CustomerOrdersDiscounts = cycle.CustomerOrdersDiscounts.KAfAdd(md.Discount).KAfSubtract(md.Added);
-                cycle.CustomerOrdersExpences = cycle.CustomerOrdersExpences.KAfAdd(md.ExpenceValue);
-                cycle.Cash = cycle.Cash.KAfAdd(md.Paid).KAfSubtract(md.ExpenceValue);
-
-
-                md.ChecKCustomerOrderConstraints();
-                await db.CustomerOrders.AddAsync(md);
-                await db.SaveChangesAsync();
-                md.ReferenceNumber = GenerateReferenceNumber("cst", md);
-                if (md.Paid > 0 || md.ExpenceValue > 0)
-                {
-                    PaymentOption payment = await db.Payments.SingleAsync(j => j.Id == md.TempPaymentId);
-                    payment.Credit = payment.Credit.KAfAdd(md.Paid).KAfSubtract(md.ExpenceValue);
-                    payment.ChecKPaymentConstraint();
-                    if (md.Paid > 0)
-                    {
-                        CustomerOrderPayment pay = new CustomerOrderPayment();
-                        pay.SessionId = md.SessionId;
-                        pay.CustomerOrderId = md.Id;
-                        pay.PaymentId = md.TempPaymentId;
-                        pay.Amount = md.Paid;
-                        d
+        }
