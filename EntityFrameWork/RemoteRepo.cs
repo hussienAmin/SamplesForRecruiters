8a07dfddfc2159b325c217684489f69d68ebe43e
Aussien A Amin <122715805+hussienAmin@users.noreply.github.com>
2024-11-13 19:20:26 +0200

    Update EntityFrameWork

---
+++ EntityFrameWork/RemoteRepo.cs
@@ -0,0 +1,22 @@
+﻿using KafApp.Config;
+using KafApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Security.Policy;
+
+
+namespace KafApp.Repo.Server
+{
+
+    public partial class ServerRepo : Irepo
+    {
+        public ServerRepo()
+        {
+            service = new(new HttpClient());
+
+        }
+        HttpClientService service;
+
